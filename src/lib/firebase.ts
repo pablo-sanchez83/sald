8c870a777cf1875397a48dd8d3fabc0307aff0a0
Pablo Sánchez <149807125+pablo-sanchez83@users.noreply.github.com>
2025-05-29 18:10:11 +0200

    firebase basic conf

---
+++ src/lib/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { 
+    API_KEY, 
+    APP_ID, 
+    AUTH_DOMAIN, 
+    MSG_SENDER_ID, 
+    PROJECT_ID, 
+    STORAGE_BUCKET 
+} from "$env/static/private";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MSG_SENDER_ID,
+  appId: APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
