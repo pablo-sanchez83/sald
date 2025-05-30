e8b1f5bd2947017973ca4d31ca7567abe532e2b8
Pablo Sánchez <149807125+pablo-sanchez83@users.noreply.github.com>
2025-05-30 14:36:49 +0200

    basic firebase conf

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,30 +1,50 @@
 // Import the functions you need from the SDKs you need
-import { 
-    API_KEY, 
-    APP_ID, 
-    AUTH_DOMAIN, 
-    MSG_SENDER_ID, 
-    PROJECT_ID, 
-    STORAGE_BUCKET 
-} from "$env/static/private";
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { writable } from "svelte/store";
+import { PUBLIC_API_KEY, PUBLIC_APP_ID, PUBLIC_AUTH_DOMAIN, PUBLIC_MSG_SENDER_ID, PUBLIC_PROJECT_ID, PUBLIC_STORAGE_BUCKET } from "$env/static/public";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MSG_SENDER_ID,
-  appId: APP_ID
+  apiKey: PUBLIC_API_KEY,
+  authDomain: PUBLIC_AUTH_DOMAIN,
+  projectId: PUBLIC_PROJECT_ID,
+  storageBucket: PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_MSG_SENDER_ID,
+  appId: PUBLIC_APP_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getFirestore();
 export const auth = getAuth(app);
 
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
