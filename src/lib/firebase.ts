5caadf91113cbb7715a5588a50dd86e5f65e8587
Pablo Sánchez <149807125+pablo-sanchez83@users.noreply.github.com>
2025-05-30 10:46:34 +0200

    basic firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,6 +8,8 @@ import {
     STORAGE_BUCKET 
 } from "$env/static/private";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +24,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
