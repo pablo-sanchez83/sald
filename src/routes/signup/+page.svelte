40333de76e56f046240349bb73c0d73123af1201
Pablo Sánchez <149807125+pablo-sanchez83@users.noreply.github.com>
2025-05-31 15:28:35 +0200

    refactor signup page: improve error handling and enhance input validation messages

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { auth, db, user } from "$lib/firebase";
     import {
-    createUserWithEmailAndPassword,
+        createUserWithEmailAndPassword,
         GoogleAuthProvider,
         signInWithPopup,
     } from "@firebase/auth";
@@ -48,8 +48,9 @@
                 salary: 0,
             });
             console.log("User signed in:", user);
-        } catch (error) {
-            console.error("Error signing in with Google:", error);
+        } catch (err) {
+            console.error("Error signing in with Google:", err);
+            error = "Error al registrarse con Google.";
         }
     }
 
@@ -66,27 +67,41 @@
                 return;
             }
 
-            const result = await createUserWithEmailAndPassword(auth, data.email, data.password)
+            const result = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password,
+            );
             const user = result.user;
             await setDoc(doc(db, "users", user.uid), {
-                    username: data.username,
-                    email: data.email,
-                    settings: {
-                        theme: "light",
-                        notifications: false,
-                    },
-                    createdAt: new Date(),
-                    salary: 0,
-        });
-        } catch(error) {
-            console.error("Error signing up:", error);
-            // ..
+                username: data.username,
+                email: data.email,
+                settings: {
+                    theme: "light",
+                    notifications: false,
+                },
+                createdAt: new Date(),
+                salary: 0,
+            });
+        } catch (err) {
+            console.error("Error signing up:", err);
+            error = "Error al crear la cuenta. Intenta con otro correo.";
         }
     }
 </script>
+
+{#if error}
+    <div class="toast toast-end">
+        <div class="alert alert-error">
+            <span>{error}</span>
+        </div>
+    </div>
+{/if}
 {#if $user}
     <div class="min-h-screen flex items-center justify-center bg-gray-100">
-        <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md text-center">
+        <div
+            class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md text-center"
+        >
             <h2 class="text-xl font-semibold mb-4">Ya has iniciado sesión</h2>
             <button
                 class="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded transition"
@@ -98,65 +113,66 @@
         </div>
     </div>
 {:else}
-    <div class="min-h-screen flex items-center justify-center bg-gray-100" >
-    <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-        <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">
-            Crear cuenta
-        </h1>
-        <form class="space-y-4" on:submit|preventDefault="{signUp}">
-            <input
-                type="text"
-                placeholder="Username"
-                class="input w-full"
-                bind:value={data.username}
-                class:input-error={!isValidUsername && isTouchedUsername}
-                class:input-success={isValidUsername}
-            />
-            <input
-                type="text"
-                placeholder="Email"
-                class="input w-full"
-                bind:value={data.email}
-                class:input-error={!isValidEmail && isTouchedEmail}
-                class:input-success={isValidEmail}
-            />
-            <input
-                type="password"
-                placeholder="Contraseña"
-                class="input w-full"
-                bind:value={data.password}
-                class:input-error={!isValidPassword && isTouchedPassword}
-                class:input-success={isValidPassword}
-            />
-            <input
-                type="password"
-                placeholder="Confirmar Contraseña"
-                class="input w-full"
-                bind:value={data.passwordConfirm}
-                class:input-error={!isEqualPassword &&
-                    !isValidPassword &&
-                    isTouchedPassword}
-                class:input-success={isEqualPassword && isValidPassword}
-            />
-            <p class="text-red-500 text-sm mt-2">{error}</p>
-            <button
-                class="mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded transition"
-                type="submit"
-            >
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
+            <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">
                 Crear cuenta
-            </button>
-        </form>
-        
-        <a href="/login" class="btn btn-soft btn-xs mt-1.5">
-            Ya tienes cuenta? Inicia sesión
-        </a>
-        <div class="flex items-center my-4">
-            <div class="flex-grow h-px bg-gray-300"></div>
-            <span class="mx-2 text-gray-400 text-sm">o</span>
-            <div class="flex-grow h-px bg-gray-300"></div>
+            </h1>
+            <form class="space-y-4" on:submit|preventDefault={signUp}>
+                <input
+                    type="text"
+                    placeholder="Username (5-15 letras/números, puede usar . o _)"
+                    class="input w-full"
+                    bind:value={data.username}
+                    class:input-error={!isValidUsername && isTouchedUsername}
+                    class:input-success={isValidUsername}
+                />
+                <input
+                    type="text"
+                    placeholder="Email válido (ej: usuario@email.com)"
+                    class="input w-full"
+                    bind:value={data.email}
+                    class:input-error={!isValidEmail && isTouchedEmail}
+                    class:input-success={isValidEmail}
+                />
+                <input
+                    type="password"
+                    placeholder="Contraseña (5-29 caracteres)"
+                    class="input w-full"
+                    bind:value={data.password}
+                    class:input-error={!isValidPassword && isTouchedPassword}
+                    class:input-success={isValidPassword}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirmar Contraseña"
+                    class="input w-full"
+                    bind:value={data.passwordConfirm}
+                    class:input-error={!isEqualPassword &&
+                        !isValidPassword &&
+                        isTouchedPassword}
+                    class:input-success={isEqualPassword && isValidPassword}
+                />
+                <p class="text-red-500 text-sm mt-2">{error}</p>
+                <button
+                    class="mt-6 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded transition"
+                    type="submit"
+                >
+                    Crear cuenta
+                </button>
+            </form>
+
+            <a href="/login" class="btn btn-soft btn-xs mt-1.5">
+                Ya tienes cuenta? Inicia sesión
+            </a>
+            <div class="flex items-center my-4">
+                <div class="flex-grow h-px bg-gray-300"></div>
+                <span class="mx-2 text-gray-400 text-sm">o</span>
+                <div class="flex-grow h-px bg-gray-300"></div>
+            </div>
+            <button class="btn btn-primary w-full" on:click={signUpWithGoogle}
+                >Continuar con Google</button
+            >
         </div>
-        <button class="btn btn-primary w-full" on:click={signUpWithGoogle}
-            >Continuar con Google</button>
     </div>
-</div>
 {/if}
